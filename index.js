1331679210ca867d793e2ac226d24ed77902fc38
CristobalMorgado <CristobalMorgado@users.noreply.github.com>
2026-07-29 02:11:43 -0400

    Ajustado DNS para entorno servidor en Render

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+if (!process.env.RENDER) {
+  try {
+    const dns = require('dns');
+    dns.setDefaultResultOrder('ipv4first');
+    dns.setServers(['8.8.8.8', '8.8.4.4']);
+  } catch (e) {}
+}
 const express  = require('express');
 const mongoose = require('mongoose');
 const cors     = require('cors');
